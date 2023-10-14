86ad75faecf6ac15d9b16d5e40ecff66677fd13e
Marina Mel <superdog04@mail.ru>
2023-10-14 13:59:08 +0300

    add FooBar

--- StudentSchool_DO/Practice_1410/Program.cs
+++ StudentSchool_DO/Practice_1410/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(FooBar(15));
+Console.WriteLine(FooBar(5));
+Console.WriteLine(FooBar(3));
+Console.WriteLine(FooBar(17));
+
+string FooBar(int num)
+{
+    if (num % 5 == 0 && num % 3 == 0)
+    {
+        return "foobar";
+    }
+    else if (num % 3 == 0)
+    {
+        return "foo";
+    }
+    else if (num % 5 == 0)
+    {
+        return "bar";
+    }
+
+    return "";
+}
