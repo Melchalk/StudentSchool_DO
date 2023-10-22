99ad58609f7dc511e02addab1d72111ba3dedd84
Marina Mel <superdog04@mail.ru>
2023-10-22 13:26:37 +0300

    done HW2

---
+++ StudentSchool_DO/HW2/Action.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+namespace HW2;
+
+internal class Action
+{
+    public string Message { get; set; }
+    public string DoneMessage { get; set; }
+
+    public Action()
+    {
+        DoneMessage = "\nДействие выполнено";
+    }
+
+    public virtual string PerformAction()
+    {
+        return DoneMessage;
+    }
+}
+
+internal class Read : Action
+{
+    readonly string filePath = @"C:\Users\Mel\Downloads\StudentSchool_DO\StudentSchool_DO\HW2\ForRead.txt";
+
+    public Read()
+    {
+        Message = "-- Выполняется чтение из файла --";
+        DoneMessage = "\nПрочтеные строки: ";
+    }
+
+    public override string PerformAction()
+    {
+        Console.Write("Введите количество строк: ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        if (!int.TryParse(Console.ReadLine(), out int countStr))
+        {
+            Menu.MistakeAndTransition();
+            return string.Empty;
+        }
+
+        StringBuilder stringBuilder = new();
+        var stringOfText = File.ReadAllText(filePath).Replace(". ", ".\n").Split('\n');
+
+        stringBuilder.Append(stringOfText[0]);
+        for (int indexStr = 1; indexStr < countStr && indexStr < stringOfText.Length; indexStr++)
+        {
+            stringBuilder.Append('\n');
+            stringBuilder.Append(stringOfText[indexStr]);
+        }
+
+        if (stringOfText.Length <  countStr)
+        {
+            stringBuilder.Append("\nВсе строки текста выведены");
+        }
+
+
+        return $"{DoneMessage}\n{stringBuilder}";
+    }
+}
+
+internal class Write : Action
+{
+    readonly string filePath = Directory.GetCurrentDirectory() + "/URL.txt";
+
+    public Write()
+    {
+        Message = "-- Выполняется запись URL --";
+        DoneMessage = "\nЗапись выполнена";
+    }
+
+    public override string PerformAction()
+    {
+        Console.Write("Введите URL: ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        File.WriteAllText(filePath, Console.ReadLine());
+
+        return DoneMessage;
+    }
+}
+
+internal class Fibonacci : Action
+{
+    public Fibonacci()
+    {
+        Message = "-- Выполняется вычисления числа Фибоначчи --";
+        DoneMessage = "\nИскомое число Фибоначчи:";
+    }
+
+    public override string PerformAction()
+    {
+        Console.Write("Введите номер числа: ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        if (!int.TryParse(Console.ReadLine(), out int countNumber) || countNumber < 1)
+        {
+            Menu.MistakeAndTransition();
+            return string.Empty;
+        }
+
+        ulong fib1 = 1, fib2 = 1;
+        countNumber -= 2;
+
+        while (countNumber > 0)
+        {
+            ulong fib_sum = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fib_sum;
+            countNumber -= 1;
+        }
+
+        return $"{DoneMessage} {fib2}";
+    }
+}
+
+internal class Exit : Action
+{
+    public bool flagOfEnd = false;
+
+    public Exit()
+    {
+        Message = "-- Выполняется выход из программы --";
+    }
+
+    public override string PerformAction()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        flagOfEnd = true;
+        return DoneMessage;
+    }
+}
---
+++ StudentSchool_DO/HW2/Menu.cs
@@ -0,0 +1,83 @@
+﻿namespace HW2;
+
+static internal class Menu
+{
+    static public Read read = new();
+    static public Write write = new();
+    static public Fibonacci fibonacci = new();
+    static public Exit exit = new();
+    static public List<Action> actions = new() { read, write, fibonacci, exit };
+    const string mistake = "\nНекорректный ввод";
+    const string tryAgain = "Выполните попытку снова";
+
+    public static int? StartMenu()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("---- Добро пожаловать в меню ----");
+        Console.WriteLine("Выберите желаемую опцию");
+        Console.WriteLine($"- Чтение: {actions.IndexOf(read)}\n- Запись: {actions.IndexOf(write)}" +
+            $"\n- Вывод числа Фибоначчи: {actions.IndexOf(fibonacci)}\n- Выход: {actions.IndexOf(exit)}");
+        Console.Write("\nНомер выбора - ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        try
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static void Choice(int numOfAction)
+    {
+        PerformingAction(actions[numOfAction]);
+
+        if (actions[numOfAction] == exit)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }
+
+        MediumMenu(numOfAction);
+    }
+
+    static void MediumMenu(int numOfAction)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\nВыберите действие: ");
+        Console.WriteLine("Повторить операцию (1)");
+        Console.WriteLine("Главное меню (2)");
+        Console.Write("\nНомер - ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        if (!(int.TryParse(Console.ReadLine(), out int meduimAction) && (meduimAction == 1 || meduimAction == 2)))
+            MistakeAndTransition();
+
+        if (meduimAction == 1)
+            Choice(numOfAction);
+    }
+
+    public static void MistakeAndTransition()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(mistake);
+        Console.WriteLine(tryAgain);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    static void PerformingAction<T>(T action) where T : Action
+    {
+        SendMessage(action.Message);
+        Console.WriteLine(action.PerformAction());
+    }
+
+    public static void SendMessage(string message)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+    }
+}
